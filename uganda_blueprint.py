31da906d6121db8cdd1e0da78f7368333d30ae2c
snazziington <amini4life@gmail.com>
2026-03-02 10:46:10 +0100

    upd8

--- uganda_blueprint.py
+++ uganda_blueprint.py
@@ -1,4 +1,4 @@
-from random import randint, choice
+from random import randint, choice # type: ignore
 from gdpc import Editor, Block
 from gdpc.geometry import placeCuboid, placeCuboidHollow
 
@@ -6,111 +6,104 @@ editor = Editor(buffering=True)
 
 buildArea = editor.getBuildArea()
 
-# region Deciding Floor Level
-
+# Finding floor
 # Load world slice of the build area
 editor.loadWorldSlice(cache=True)
 
 # Get heightmap
-heightmap = editor.worldSlice.heightmaps["MOTION_BLOCKING_NO_LEAVES"]
+heightmap = editor.worldSlice.heightmaps["MOTION_BLOCKING_NO_LEAVES"] # type: ignore
 
-# i guess y is equal to the average heighmap??????? idk man
-y = heightmap[5,5]
+#  y = Foundation height
+y = heightmap[0, 0] # + 3 is temporary while i do blueprints
 
-# endregion
+garageLeft = randint(0, 1)
+
+# x & z coordinates to build the house
+houseX = buildArea.offset.x + 1
+houseZ = buildArea.offset.z + 1
+
+height = 3
+
+# LIVINGROOM
+# Coordinates
+# Width
+lRoomX1 = houseX + 10 # always 10 blocks from edge
+lRoomX2 = lRoomX1 + 10
+
+#Depth
+lRoomZ1 = houseZ # + 10 # always 10 blocks from edge
+lRoomZ2 = lRoomZ1 + randint(12, 16)
+
+# Livingroom Palette
+livingRoomWall = Block("white_concrete")
+livingRoomFloor = Block("white_wool")
+
+# Livingroom Wall
+placeCuboidHollow(editor, (lRoomX1, y, lRoomZ1), (lRoomX2, y + height, lRoomZ2), livingRoomWall)
+
+# Livingroom Floor
+placeCuboidHollow(editor, (lRoomX1, y, lRoomZ1), (lRoomX2, y, lRoomZ2), livingRoomFloor)
+
+# GARAGE
+# Coordinates
+# Width
+if garageLeft == 1:
+    garageX1 = lRoomX2
+    garageX2 = garageX1 + randint(6, 8)
+    print("Garage is left")
+
+else:
+    garageX1 = lRoomX1
+    garageX2  = garageX1 - randint(6, 8)
+    print("Garage is right")
+
+# Depth
+garageZ1 = int((lRoomZ1 * .7 + lRoomZ2 * 0.3))
+garageZ2 = lRoomZ2
+
+#Garage Palette
+garageWall = Block("gray_concrete")
+garageFloor = Block("gray_wool")
+
+# Garage Wall + Door
+placeCuboidHollow(editor, (garageX1, y, garageZ1), (garageX2, y + height, garageZ2), garageWall)
+garageDoor = Block("pale_oak_door", {"facing": "west", "hinge": "left"})
+editor.placeBlock((garageX1, y+1, garageZ2 - 2), garageDoor)
+
+# Garage Floor
+placeCuboidHollow(editor, (garageX1, y, garageZ1), (garageX2, y, garageZ2), garageFloor)
+
+# region HALLWAY
+# Coordinates
+# Width --- Should be dependent on the size of the rooms!
+# Or actually; the rooms should be dependent on the size of the hallways.
+hallwayWidth  = 3
+if garageLeft == 1:
+    hallwayX1 = lRoomX1
+    hallwayX2 = hallwayX1 - 10 # randint(6, 8)
+
+else:
+    hallwayX1 = lRoomX2
+    hallwayX2 = hallwayX1 + 10 # randint(6, 8)
+
+#Depth
+hallwayZ1 = (lRoomZ1 + lRoomZ2) / 2
+hallwayZ2 = hallwayZ1 + 3
+
+# Hallway Palette
+hallwayWall = Block("blue_concrete")
+hallwayFloor = Block("blue_wool")
+
+# Hallway Wall
+placeCuboidHollow(editor, (hallwayX1, y, hallwayZ1), (hallwayX2, y + height, hallwayZ2), hallwayWall) # type: ignore
+
+# Hallway Floor
+placeCuboidHollow(editor, (hallwayX1, y, hallwayZ1), (hallwayX2, y, hallwayZ2), hallwayFloor) # type: ignore
 
 
+# Removes top layer so I can see blueprint
+placeCuboid(editor, (houseX, y + 3, houseZ), (houseX + 30, y + 3, houseZ + 30), Block("air"))
 
-
-# region Deciding X/Z Coords
-x = buildArea.offset.x + 1
-z = buildArea.offset.z + 1
-
-# endregion
-
-# region Black Palette
-# Floor
-floorPalette = [
-    Block("stone_bricks"),
-    Block("cracked_stone_bricks"),
-    Block("cobblestone"),
-]
-
-# Wall
-wallBlock = choice([
-    Block("white_terracotta"),
-    Block("brown_terracotta"),
-    Block("black_terracotta"),
-])
-print(f"Chosen wall block: {wallBlock}")
-
-# endregion
-
-
-# region Room Sizes
-houseHeight  = randint(6, 15)
-air = Block("air")
-
-widthLRoom   = randint(3, 7) * 2 - 1 # odd number
-depthLRoom   = randint(8, 15)
-
-widthKitchen = randint(3, 7) * 2 - 1 # odd number
-depthKitchen = randint(8, 15)
-wallKitchen = Block("pink_terracotta")
-
-
-
-# endregion
-
-
-# i will eventually want to generate the livingroom away from the edges of the buildarea so i can have rooms either
-# side of the livingroom.
-# i will also want to make some potted plants in the front near the windows
-# dear god i'm gonna have to make windows
-# i will need to make if statements maybe. like if divisible by xyz and bigger than xyz then do this pattern of windows blah blah
-# i will have to figure out a formula yippeeeeeeeeeeeeeeeee!!
-
-# Build livingroom
-placeCuboidHollow(editor, (x, y, z), (x + widthLRoom, y + houseHeight, z + depthLRoom), wallBlock)
-placeCuboid(editor, (x, y, z), (x + widthLRoom, y, z + depthLRoom), floorPalette)
-
-# maybe I could even redefine x and Z? or like. have a xLRoom and zLRoom variable fo rthe boundaries of the
-# lroom? so the formulas aren't that long + wordy
-
-# what i could also do is 1. generate a random number between 3 and 7. 2. create that number of rooms 3. each
-# room is assignmened a different purpose (lroom/bedroom/kitchen/bathroom?), room 1 is always livingroom?
-# and that could be how things are generated? so instead of widthKitchen itd be widthRoom1
-# but then there's the issue of room sizes not making sense 9e.g., no one needs a bathroom the size of a like, 
-# master bedroom lmao.
-
-
-# Build kitchen
-placeCuboidHollow(editor, (x + widthLRoom, y, z),
-                          (x + widthKitchen + widthLRoom, y + houseHeight, z + depthKitchen),
-                           wallKitchen)
-
-# clear the space between the kitchen 
-placeCuboid(editor, (x + widthLRoom, y, z),
-                    (x + widthLRoom, y + houseHeight - 1, z + depthLRoom),
-                     air)
-placeCuboid(editor, (x, y, z + depthLRoom), (x+widthKitchen, y, z), Block("white_concrete"))
-
-# Build roof: loop through distance from the middle
-# range(a, b) = number of roof rows - 1
-for dx in range(1, 4):
-    # height of roof rows
-    yy = y + houseHeight + 2 - dx
-
-    # Build row of stairs blocks
-    leftBlock  = Block("oak_stairs", {"facing": "east"})
-    rightBlock = Block("oak_stairs", {"facing": "west"})
-    placeCuboid(editor, (x+2-dx, yy, z-1), (x+2-dx, yy, z+depthLRoom+1), leftBlock)
-    placeCuboid(editor, (x+2+dx, yy, z-1), (x+2+dx, yy, z+depthLRoom+1), rightBlock)
-
-# build the top row of the roof
-yy = y + houseHeight + 1
-placeCuboid(editor, (x+2, yy, z-1), (x+2, yy, z+depthLRoom+1), Block("oak_planks"))
-
-# Add a door
-doorBlock = Block("oak_door", {"facing": "south", "hinge": "left"})
-editor.placeBlock((x+2, y+1, z), doorBlock)
+# Add double doors
+editor.placeBlock((lRoomX1+2, y+1, lRoomZ1), Block("oak_door", {"facing": "south", "hinge": "right"}))
+editor.placeBlock((lRoomX1+3, y+1, lRoomZ1), Block("oak_door", {"facing": "south", "hinge": "left"}))
